1d6b1d79bfa0589c2e9c31f3d0f7800b3e993559
mechaashik <mechaashik@gmail.com>
2022-05-17 18:47:50 +0600

    Putting json file to database and find data from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,10 +8,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.awau4.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+
+        
+        app.get('/service', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    } 
+
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello From Doctors Portal Server!')
+    res.send('Hello From Doctors Portal Server!')
 })
 
 app.listen(port, () => {
-  console.log(`Doctors App listening on port ${port}`)
+    console.log(`Doctors App listening on port ${port}`)
 })
