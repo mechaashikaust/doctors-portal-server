060f5083123f8073dcb4e50074a66c1eb710ad78
mechaashik <mechaashik@gmail.com>
2022-05-20 12:35:21 +0600

    76 Done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+var nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
+
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const app = express();
@@ -15,6 +18,81 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// {4} My Appointemnts with verifying JWT
+
+function verifyJWT(req, res, next) {
+    //1///////////////////////////////////////
+    const authHeader = req.headers.authorization;
+
+    //2///////////////////////////////////////
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized' });
+    }
+    //3////////////////////////////////////////
+    const token = authHeader.split(' ')[1];
+
+    //4////////////////////////////////////////
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        req.decoded = decoded;
+
+        //5/////////////////////////////////////////
+        next();
+
+    });
+
+}
+
+
+
+// Send GRID // start
+
+const emailSenderOptions = {
+    auth: {
+        api_key: process.env.EMAIL_SENDER_KEY
+    }
+}
+
+const emailClient = nodemailer.createTransport(sgTransport(emailSenderOptions));
+
+function sendAppointmentEmail(booking) {
+    const { patient, patientName, treatment, date, slot } = booking;
+
+    var email = {
+        from: 'mechaashik@gmail.com',
+        to: patient,
+        subject: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+        text: `Your Appointment for ${treatment} is on ${date} at ${slot} is Confirmed`,
+        html: `
+        <div>
+          <p> Hello ${patientName}, </p>
+          <h3>Your Appointment for ${treatment} is confirmed</h3>
+          <p>Looking forward to seeing you on ${date} at ${slot}.</p>
+          
+          <h3>Our Address</h3>
+          <p>Andor Killa Bandorban</p>
+          <p>Bangladesh</p>
+          <a href="https://web.programming-hero.com/">unsubscribe</a>
+        </div>
+      `
+    };
+
+    emailClient.sendMail(email, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log('Message sent: ', info);
+        }
+    });
+
+}
+
+// Send GRID // END
+
+
 async function run() {
     try {
 
@@ -25,34 +103,28 @@ async function run() {
         const bookingCollection = client.db('doctors_portal').collection('bookings');
         // Users Collection
         const usersCollection = client.db('doctors_portal').collection('users');
+        // Doctors Collection
+        const doctorCollection = client.db('doctors_portal').collection('doctors');
 
 
-        // {4} My Appointemnts with verifying JWT
+        // Verifying admin for doctors         (after image uploading) 
+        const verifyAdmin = async (req, res, next) => {
 
-        function verifyJWT(req, res, next) {
-            //1///////////////////////////////////////
-            const authHeader = req.headers.authorization;
+            //Copied from jwt
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                //Copied from jwt // END
 
-            //2///////////////////////////////////////
-            if (!authHeader) {
-                return res.status(401).send({ message: 'UnAuthorized' });
-            }
-            //3////////////////////////////////////////
-            const token = authHeader.split(' ')[1];
-
-            //4////////////////////////////////////////
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-                if (err) {
-                    return res.status(403).send({ message: 'Forbidden Access' });
-                }
-                req.decoded = decoded;
-
-                //5/////////////////////////////////////////
                 next();
-
-            });
-
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
         }
+
+
+
 
         // app.get('/', async (req, res) => {
 
@@ -62,9 +134,11 @@ async function run() {
 
 
         // Get All Services From DB
+        // {8} Add Doctors data getting  => .project({ name: 1 })
+
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({ name: 1 });
             const services = await cursor.toArray();
             res.send(services);
         })
@@ -82,27 +156,16 @@ async function run() {
 
         // {6} Admin
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
 
-            const requester = req.decoded.email;
-            const requesterAccount = await usersCollection.findOne({ email: requester });
-            if (requesterAccount.role === 'admin') {
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
 
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-                const result = await usersCollection.updateOne(filter, updateDoc);
-
-                res.send({ result });
-
-            }
-            else {
-
-                return res.status(403).send({ message: 'Forbidden' });
-
-            }
+            res.send({ result });
 
 
         })
@@ -178,17 +241,17 @@ async function run() {
 
         /*
             * API Naming Convention
-
+ 
             * app.get('/booking') // get all Bookings in this collection. or get more than one or by filter
-
+ 
             * app.get('/booking/:id')   //  Get a specific Booking 
-
+ 
             * app.post('/booking')      //  Add a new specific Booking
-
+ 
             * app.put('/booking/:id)    // upsert => update (if exists) or insert (if desn't exist)
         
             * app.patch('/booking/:id)  //  Update a specific Booking
-
+ 
             * app.delete('/booking/:id) //  Delete a specific Booking
         */
 
@@ -224,8 +287,42 @@ async function run() {
 
 
             const result = await bookingCollection.insertOne(booking);
+
+            // SendGrid (mail verification)
+            sendAppointmentEmail(booking);
+
             return res.send({ success: true, result });
+        });
+
+
+        // {10} Getting all Doctors 
+        app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctors = await doctorCollection.find().toArray();
+            res.send(doctors);
         })
+
+        // {9} Doctor Adding & image Uploading
+
+        app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctor = req.body;
+            const result = await doctorCollection.insertOne(doctor);
+            res.send(result);
+        })
+
+        // {11} Delete Doctor
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await doctorCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+
+
+
+
+
+
 
     }
 
